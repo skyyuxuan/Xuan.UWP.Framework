7a1d5362ef96541d7f1d35143efa2cfee7d8d954
Xuan YU <j-xuanyu@microsoft.com>
2016-06-22 18:37:40 +0800

    Create StorageCache

---
+++ Xuan.UWP.Framework/Xuan.UWP.Framework/Image/Cache/CacheGenerator/ICacheGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xuan.UWP.Framework.Image.Cache
+{
+    public interface ICacheGenerator
+    {
+        string GeneratorName(string name);
+    }
+
+}
