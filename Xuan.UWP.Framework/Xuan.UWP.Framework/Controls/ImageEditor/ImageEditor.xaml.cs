b56216d3073cfd85b8d0344a5caf94d3dd45571d
xuanyu <xuanyu@westwin.com>
2016-11-17 18:53:45 +0800

    init imageeditor

---
+++ Xuan.UWP.Framework/Xuan.UWP.Framework/Controls/ImageEditor/ImageEditor.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Xuan.UWP.Framework.Controls.ImageEditor
+{
+    public sealed partial class ImageEditor : UserControl
+    {
+        public ImageEditor()
+        {
+            this.InitializeComponent();
+        } 
+      
+        private void MainCanvasControl_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
+        {
+
+        }
+    }
+}
