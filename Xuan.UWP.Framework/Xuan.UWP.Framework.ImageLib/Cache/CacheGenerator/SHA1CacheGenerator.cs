a53f800cd8a61265b4b49aa1d3cab11b03509357
xuanyu <xuanyu@westwin.com>
2017-10-25 12:43:16 +0800

    fix

--- Xuan.UWP.Framework/Xuan.UWP.Framework.ImageLib/Cache/CacheGenerator/SHA1CacheGenerator.cs
+++ Xuan.UWP.Framework/Xuan.UWP.Framework.ImageLib/Cache/CacheGenerator/SHA1CacheGenerator.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xuan.UWP.Framework.Utils;
 
 namespace Xuan.UWP.Framework.ImageLib.Cache {
     public class SHA1CacheGenerator : ICacheGenerator {
         public string GeneratorName(string name) {
-            return Xuan.UWP.Framework.Utils.SH1Util.ComputeHash(name);
+            return SH1Util.ComputeHash(name);
         }
     }
 }
--- Xuan.UWP.Framework/Xuan.UWP.Framework.TestApp/MainPage.xaml.cs
+++ Xuan.UWP.Framework/Xuan.UWP.Framework.TestApp/MainPage.xaml.cs
@@ -7,46 +7,37 @@ using Xuan.UWP.Framework.Common;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Xuan.UWP.Framework.TestApp
-{
+namespace Xuan.UWP.Framework.TestApp {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+    public sealed partial class MainPage : Page {
+        public MainPage() {
             this.InitializeComponent();
         }
 
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
-        {
+        protected async override void OnNavigatedTo(NavigationEventArgs e) {
             base.OnNavigatedTo(e);
-            //using (var stream = await ImageLib.ImageLoader.GetInstance.GetImageStreamAsync(@"http://ecx.images-amazon.com/images/I/512Pd6birKL.jpg",
-            //    null, new System.Threading.CancellationToken()))
-            //{
-            //    if (stream != null && stream.Size > 0)
-            //    {
-            //        var bit = new BitmapImage();
-            //        await bit.SetSourceAsync(stream);
-            //        img.Source = bit;
-            //    }
-            //}
+            using (var stream = await ImageLib.ImageLoader.GetInstance.GetImageStreamAsync(@"http://ecx.images-amazon.com/images/I/512Pd6birKL.jpg",
+                null, new System.Threading.CancellationToken())) {
+                if (stream != null && stream.Size > 0) {
+                    var bit = new BitmapImage();
+                    await bit.SetSourceAsync(stream);
+                    img.Source = bit;
+                }
+            }
             List<ImageItem> imageList = new List<ImageItem>();
-            for (int i = 0; i < 100; i++)
-            {
+            for (int i = 0; i < 100; i++) {
                 imageList.Add(new ImageItem() { Url = @"http://ecx.images-amazon.com/images/I/512Pd6birKL.jpg" });
             }
             listView.ItemsSource = imageList;
         }
 
-        public class ImageItem : DataModelBase
-        {
+        public class ImageItem : DataModelBase {
             private string _url;
-            public string Url
-            {
+            public string Url {
                 get { return _url; }
-                set { SetProperty(ref _url, value); } 
+                set { SetProperty(ref _url, value); }
             }
         }
     }
