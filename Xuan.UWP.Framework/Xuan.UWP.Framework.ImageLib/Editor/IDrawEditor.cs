bf9649d7bdfafc23c56270372a9d147fa95bcdc4
xuanyu <xuanyu@westwin.com>
2017-10-31 15:00:21 +0800

    init ImageEditor

---
+++ Xuan.UWP.Framework/Xuan.UWP.Framework.ImageLib/Editor/IDrawEditor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Graphics.Canvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xuan.UWP.Framework.ImageLib.Editor {
+    public interface IDrawEditor {
+        void Draw(CanvasDrawingSession graphics);
+    }
+}
---
+++ Xuan.UWP.Framework/Xuan.UWP.Framework.ImageLib/Editor/ImageEditor.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Graphics.Canvas;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Xuan.UWP.Framework.ImageLib.Editor {
+    public sealed partial class ImageEditor : UserControl {
+
+        public ImageEditor() {
+            this.InitializeComponent();
+        }
+
+        private CanvasBitmap _imageBitmap;
+
+        private void OnDraw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args) {
+
+        }
+
+        private CanvasRenderTarget GetRenderTarget() {
+            var width = canvasControl.ActualHeight;
+            var height = canvasControl.ActualHeight;
+            CanvasDevice device = CanvasDevice.GetSharedDevice();
+            CanvasRenderTarget target = new CanvasRenderTarget(device, (float)width, (float)height, 96);
+            using (CanvasDrawingSession graphics = target.CreateDrawingSession()) {
+
+            }
+            return target;
+        }
+
+        private void DrawImage() {
+            var rec = GetImageDrawingRect();  
+        }
+        private Rect GetImageDrawingRect() {
+            var rect = new Rect();
+            return rect;
+        }
+    }
+}
